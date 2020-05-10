9f22dce910b22e6816dad10d576fd1b770c64a6c
ruchika <ruchika6698@gmail.com>
2020-05-10 20:05:52 +0530

    Added:JSON Server API

---
+++ employees.js
@@ -0,0 +1,31 @@
+var faker = require('faker')
+
+	function generateEmployees()
+	{
+
+   	var employees=[]
+
+   	for(var id=1;id<=50;id++)
+   	{
+      	var firstName = faker.name.firstName();
+      	var lastName = faker.name.lastName();
+      	var email = faker.internet.email();
+      	var phoneNumberFormat = faker.phone.phoneNumberFormat();
+      	var companyName = faker.company.companyName();
+      	var city = faker.address.city();
+
+      	employees.push({
+         	"id":id,
+         	"First_name" : firstName,
+         	"Last_name":lastName,
+         	"email" : email,
+         	"Mobile_Number" : phoneNumberFormat,
+         	"Company" : companyName,
+         	"City" : city
+      	})
+   	}
+
+   	return { "employees" : employees }
+	}
+
+module.exports = generateEmployees
